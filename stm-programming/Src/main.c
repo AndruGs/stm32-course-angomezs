1aeab370b290c5d6aed2629c35b803d6aeb2b1f5
Andru Alexis Gomez S <angomezs@unal.edu.co>
2026-06-26 13:36:16 -0500

    week-01: complete week-00 exercises 0.1-0.8

--- stm-programming/Src/main.c
+++ stm-programming/Src/main.c
@@ -1,17 +1,74 @@
 #include <stdint.h>
+#include <limits.h>
 #include "stm32f4xx.h"
 #include "stm32f411xe.h"
 
+// Andru Gomez
+// Taller V - Week 00
+
 int main(void)
 {
-    uint8_t my_variable = 42;
+    // Ejercicio 0.1
+    // uint8_t my_variable = 42;
 
-    my_variable = 55;
+    // Ejercicio 0.2
+    // uint8_t dec = 65;
+    // uint8_t hex = 0x41;
+    // uint8_t bin = 0b01000001;
 
-    while(1)
+    // Ejercicio 0.3
+    // uint8_t  a = 255;
+    // uint16_t b = 255;
+    // uint32_t c = 255;
+    // uint8_t  d = 256;
+    // uint8_t  e = 257;
+
+    // Ejercicio 0.4
+    // uint8_t a = 200;
+    // int8_t  b = 200;
+    // int8_t  c = -1;
+
+     // Ejercicio 0.5
+     // int8_t x = 127;
+     // x = x + 1;
+
+     // uint8_t y = 255;
+     // y = y + 1;
+
+     // int8_t int8_max = INT8_MAX;
+     // int8_t int8_min = INT8_MIN;
+     // uint8_t uint8_max = UINT8_MAX;
+     // uint16_t uint16_max = UINT16_MAX;
+
+     // Ejercicio 0.6
+     // uint8_t r1 = 0x0F + 0x01;
+     // uint8_t r2 = 0xFF + 0x01;
+     // uint8_t r3 = 0xA0 + 0x5F;
+     // uint8_t r4 = 0xA0 + 0x60;
+
+     // Ejercicio 0.7
+     // uint8_t x1 = 0x01;
+     // uint8_t x2 = 0x02;
+     // uint8_t x3 = 0x04;
+     // uint8_t x4 = 0x08;
+     // uint8_t x5 = 0x10;
+     // uint8_t x6 = 0x20;
+     // uint8_t x7 = 0x40;
+     // uint8_t x8 = 0x80;
+
+     // Ejercicio 0.8
+     uint8_t a = 5;
+     uint8_t b = 0;
+     uint8_t c = 255;
+
+     uint8_t r1 = !a;
+     uint8_t r2 = !b;
+     uint8_t r3 = !c;
+     uint8_t r4 = ~c;
+     uint8_t r5 = ~a;
+
+    while (1)
     {
 
     }
-
-    return 0;
 }
